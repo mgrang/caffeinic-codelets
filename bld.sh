c457f079c2c357571e1fea152482943031ad9174
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-04-22 19:02:45 -0700

    Let bld.sh default to latest edited src file by default

--- bld.sh
+++ bld.sh
@@ -1,4 +1,11 @@
-clang++ -std=c++11 $1
+filename=$1
+if [[ -z $filename ]]; then
+  filename=`ls *.cpp | tail -1`
+fi
+
+echo "Building $filename:"
+clang++ -std=c++11 $filename
+
 if [[ $? -eq 0 ]]; then
   ./a.out
 fi
