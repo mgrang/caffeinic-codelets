d0953a885ef1e3fe6cf87a692be192d27b4742c1
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-08-28 21:44:09 -0700

    added longest-common-subsequence.cpp

---
+++ longest-common-subsequence.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int lcs(string s, string t) {
+  int n = s.length();
+  int m = t.length();
+
+  int T[n + 1][m + 1];
+
+  for (int i = 0; i <= n; ++i)
+    T[0][i] = 0;
+
+  for (int i = 0; i <= m; ++i)
+    T[i][0] = 0;
+
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      T[i][j] = (s[i] == t[j]) ?
+                1 + T[i - 1][j - 1] :
+                std::max(T[i - 1][j], T[i][j - 1]);
+    }
+  }
+  return T[n][m];
+}
+
+int main() {
+  string s = "logarithm";
+  string t = "algorithm";
+
+  cout << "LCS between " << s << " and " << t
+       << " is: " << lcs(s, t) << "\n";
+}
