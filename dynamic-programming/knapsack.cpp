387c0e7166183882d8b712b16a2918b5bad951c8
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-09-01 20:01:19 -0700

    added knapsack.cpp

---
+++ dynamic-programming/knapsack.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+typedef struct {
+  int maxVal;
+  std::vector<int> items;
+} KP;
+
+KP knapsack(int target, std::vector<int> wts, std::vector<int> vals) {
+  int T[wts.size() + 1][target + 1];
+
+  for (int i = 0; i <= wts.size(); ++i) {
+    for(int j = 0; j <= target; ++j) {
+      if (i == 0 || j == 0)
+        T[i][j] = 0;
+
+      else if (j < wts[i - 1])
+        T[i][j] = T[i - 1][j];
+
+      else
+        T[i][j] = std::max(T[i - 1][j],
+                           vals[i - 1] + T[i - 1][j - wts[i - 1]]);
+    }
+  }
+
+  std::vector<int> items;
+  int i = wts.size();
+  int j = target;
+  while (i && j && T[i][j]) {
+    if (T[i][j] == vals[i - 1] + T[i - 1][j - wts[i - 1]]) {
+      items.insert(items.begin(), wts[i - 1]);
+      j -= wts[i - 1];
+    }
+    --i;
+  }
+
+  return KP {T[wts.size()][target], items};
+}
+
+int main() {
+  int target = 6;
+  std::vector<int> wts = {1, 3, 4, 5};
+  std::vector<int> vals = {1, 4, 5, 7};
+
+  KP k = knapsack(target, wts, vals);
+
+  cout << "Max val: " << k.maxVal << "\n";
+  cout << "Wts.: ";
+  for (auto w : k.items)
+    cout << w << " ";
+  cout << "\n";
+}
