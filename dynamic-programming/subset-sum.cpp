6facd64a35a9f46a445724dbf9faeab8e0f5fb86
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-09-02 21:13:44 -0700

    added subset-sum.cpp

---
+++ dynamic-programming/subset-sum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+std::vector<int> subsetSum(int sum, std::vector<int> nums) {
+  bool T[nums.size() + 1][sum + 1];
+
+  int row = -1;
+
+  for(int i = 0; i <= nums.size(); ++i) {
+    for (int j = 0; j <= sum; ++j) {
+      if (j == nums[i - 1])
+        T[i][j] = true;
+      else if (i == 0)
+        T[i][j] = false;
+      else if (j == 0)
+        T[i][j] = true;
+      else if (j < nums[i - 1])
+        T[i][j] = T[i - 1][j];
+      else
+        T[i][j] = T[i - 1][j - nums[i - 1]];
+      }
+
+    if (T[i][sum]) {
+      row = i;
+      break;
+    }
+  }
+
+  std::vector<int> subset;
+  if (row > -1) {
+    int col = sum;
+
+    while (row && col && T[row][col]) {
+      while (row && T[row - 1][col])
+        --row;
+
+      if (row)
+        subset.insert(subset.begin(), nums[row - 1]);
+
+      col -= nums[row - 1];
+      --row;
+    }
+  }
+
+  return subset;
+}
+
+int main() {
+  int sum = 19;
+  std::vector<int> nums {1, 3, 4, 5, 7};
+
+  cout << "Target sum: " << sum << "\n";
+  cout << "Input: ";
+  for (auto I : nums)
+    cout << I << " ";
+  cout << "\n";
+
+  if (sum == 0) {
+    cout << "Subset sum: {}\n";
+    return 0;
+  }
+
+  std::vector<int> S = subsetSum(sum, nums);
+  if (S.size()) {
+    cout << "Subset sum: ";
+    for (auto I : S)
+      cout << I << " ";
+  } else
+    cout << "Subset sum does not exist";
+  cout << "\n";
+}
