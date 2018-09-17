2b2c80bcd177e3e008f133e5c7c7abaffe2839d8
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-09-16 17:50:20 -0700

    added maximum-sum-non-adjacent-subsequence.cpp

--- dynamic-programming/maximum-sum-non-adjacent-subsequence.cpp
+++ dynamic-programming/maximum-sum-non-adjacent-subsequence.cpp
@@ -2,30 +2,32 @@
 #include <vector>
 using namespace std;
 
-std::pair<int, std::vector<int>> maxSumIncSub(std::vector<int> nums) {
+std::pair<int, std::vector<int>> maxSumNonAdjSub(std::vector<int> nums) {
   int sum[nums.size()];
   int prev[nums.size()];
-  int maxSum = nums[0];
   int maxIdx = 0;
+  int maxSum = nums[0];
 
-  sum[0] = nums[0];
-  prev[0] = -1;
-
-  for (int i = 1; i < nums.size(); ++i) {
+  for (int i = 0; i < nums.size(); ++i) {
     sum[i] = nums[i];
     prev[i] = -1;
- 
-    for (int j = 0; j < i; ++j) {
-      if (nums[j] < nums[i] &&
-          nums[i] + sum[j] > sum[i]) {
-        sum[i] = nums[i] + sum[j];
-        prev[i] = j;
-      }
 
-      if (sum[i] > maxSum) {
-        maxSum = sum[i];
-        maxIdx = i;
+    if (i > 1) {
+      int a = sum[i] + sum[i - 1] - nums[i - 1];
+      int b = sum[i] + sum[i - 2];
+
+      if (a > b) {
+        sum[i] = a;
+        prev[i] = prev[i - 1];
+      } else {
+        sum[i] = b;
+        prev[i] = i - 2;
       }
+    }
+
+    if (sum[i] > maxSum) {
+      maxSum = sum[i];
+      maxIdx = i;
     }
   }
 
@@ -44,10 +46,10 @@ void test(std::vector<int> nums) {
     cout << i << " ";
   cout << "\n";
 
-  auto res = maxSumIncSub(nums);
+  auto res = maxSumNonAdjSub(nums);
 
   cout << "Max sum: " << res.first << "\n";
-  cout << "Max sum increasing sequence: ";
+  cout << "Max sum non-adjacent sequence: ";
   for (auto i : res.second)
     cout << i << " ";
   cout << "\n";
@@ -62,5 +64,6 @@ int main() {
   test({1, 2, 3, 4, 5});
   test({1, 0, 2, -1});
   test({-1, -2, -3});
+  test({-3, -2, -1});
   test({-3, -2, -1});
 }
