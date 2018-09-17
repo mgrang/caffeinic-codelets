c3859d9492b662ef004ebb7a553ce88babcaa45a
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-09-16 21:35:29 -0700

    added max-size-rectangle-ones.cpp

---
+++ dynamic-programming/max-size-rectangle-ones.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxRectInHist(std::vector<int> hist) {
+  hist.push_back(0);
+
+  std::vector<int> stack;
+  stack.push_back(0);
+  int maxArea = hist[0];
+
+  for (int i = 1; i < hist.size(); ++i) {
+    int top = stack.back();
+
+    while (stack.size() && hist[i] < hist[top]) {
+      stack.pop_back();
+      int newTop = stack.back();
+
+      int currArea;
+      if (stack.size() == 0)
+        currArea = hist[top] * i;
+      else
+        currArea = hist[top] * (i - newTop - 1);
+
+      if (currArea > maxArea)
+        maxArea = currArea;
+
+      top = newTop;
+    }
+    stack.push_back(i);
+  }
+
+  return maxArea;
+}
+
+int maxArea(std::vector<std::vector<int>> matrix) {
+  int maxArea = 0;
+
+  std::vector<int> hist(matrix[0].size());
+  for (auto rows : matrix) {
+    for (int i = 0; i < rows.size(); ++i)
+      if (rows[i])
+        ++hist[i];
+      else
+        hist[i] = 0;
+
+    int currMax = maxRectInHist(hist);
+    if (currMax > maxArea)
+      maxArea = currMax;
+  }
+  return maxArea;
+}
+
+void test(std::vector<std::vector<int>> matrix) {
+  cout << "Input:\n";
+  for (auto rows : matrix) {
+    for (auto i : rows)
+      cout << i << " ";
+    cout << "\n";
+  }
+
+  cout << "Max area of rectangle with all 1's: " << maxArea(matrix);
+  cout << "\n";
+}
+
+int main() {
+  test({{1, 1, 1, 0}, {0, 1, 0, 1}, {1, 1, 0, 1}});
+  test({{1}});
+  test({{}});
+  test({{1, 0, 0, 1, 1, 1}, {1, 0, 1, 1, 0, 1}, {0, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 1}});
+  test({{1, 1, 1}, {1, 1, 1}, {1, 1, 1}});
+  test({{1, 1, 1}, {1, 0, 1}, {1, 1, 1}});
+  test({{0, 0, 0}, {0, 0, 0}, {0, 0, 0}});
+}
+
