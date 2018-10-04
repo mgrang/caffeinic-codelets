9e920fc2061f1e91f7340fabba90dd377b862aa0
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-10-03 21:12:45 -0700

    Added max-sub-square-matrix.cpp

---
+++ dynamic-programming/max-sub-square-matrix.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubSquareMatrix(std::vector<std::vector<int>> matrix) {
+  int maxSize = 0;
+  int n = matrix.size();
+  int m = matrix[0].size();
+  int T[n][m];
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      if (i == 0 || j == 0 || matrix[i][j] == 0)
+        T[i][j] = matrix[i][j];
+      else
+        T[i][j] = 1 + std::min(T[i - 1][j - 1],
+                               std::min(T[i - 1][j], T[i][j - 1]));
+      if (T[i][j] > maxSize)
+        maxSize = T[i][j];
+    }
+  }
+  return maxSize;
+}
+
+void test(std::vector<std::vector<int>> matrix) {
+  cout << "Input:\n";
+  for (auto rows : matrix) {
+    for (auto i : rows)
+      cout << i << " ";
+    cout << "\n";
+  }
+
+  auto res = maxSubSquareMatrix(matrix);
+  cout << "Max size sub-square matrix: " << res << "x" << res;
+  cout << "\n";
+}
+
+int main() {
+  test({{1, 1, 1, 0}, {0, 1, 0, 1}, {1, 1, 0, 1}});
+  test({{1}});
+  test({{}});
+  test({{1, 0, 0, 1, 1, 1}, {1, 0, 1, 1, 0, 1}, {0, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 1}});
+  test({{1, 1, 1}, {1, 1, 1}, {1, 1, 1}});
+  test({{1, 1, 1}, {1, 0, 1}, {1, 1, 1}});
+  test({{0, 0, 0}, {0, 0, 0}, {0, 0, 0}});
+  test({{0, 0, 1, 1, 1}, {1, 0, 1, 1, 1}, {0, 1, 1, 1, 1}, {1, 0, 1, 1, 1}});
+}
